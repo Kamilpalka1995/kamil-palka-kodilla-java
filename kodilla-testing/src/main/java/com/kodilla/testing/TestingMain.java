69f9b85f42a4f429c49ff9d1d705ebd2414f5be3
Kamil Palka <dudek95.1995@gmail.com>
2026-03-24 16:20:35 +0100

    Test Driven Development-fix

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,12 +1,20 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
 
 public class TestingMain {
 
     public static void main(String[] args) {
-
+        Calculator calculator = new Calculator();
+        if (calculator.add(5, 4) == 9) {
+            System.out.println("Test add is ok");
+        } else {
+            System.out.println("Test is not ok");
+        }
+        if (calculator.substract(5, 4) == 1) {
+            System.out.println("Test substract is ok");
+        } else {
+            System.out.println("Test substract is not ok");
+        }
     }
 }
