3434eaf564d4e6258a30c5b2f647aede10026814
Kamil Palka <dudek95.1995@gmail.com>
2026-03-24 16:24:35 +0100

    Module 6.3-fix

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,4 @@
-package collection;
+package com.kodilla.testing.collection;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,9 +7,9 @@ public class OddNumbersExterminator {
     public List<Integer> exterminate(List<Integer> numbers) {
         List<Integer> result = new ArrayList<>();
 
-       for(Integer random : numbers) {
-           if(random % 2 == 0) {
-               result.add(random);
+       for(Integer number : numbers) {
+           if(number % 2 == 0) {
+               result.add(number);
            }
        }
        return result;
